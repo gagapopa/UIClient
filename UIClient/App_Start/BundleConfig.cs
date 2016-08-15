cda4ff80e8124617dadc11530500ca1cdc7f4551
gagapopa <gagapopa@ya.ru>
2016-08-15 15:39:04 +0700

    Add cookiejs

--- UIClient/App_Start/BundleConfig.cs
+++ UIClient/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace UIClient
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/cookiejs").Include(
+                        "~/Scripts/js-cookie/js.cookie.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -22,6 +25,10 @@ namespace UIClient
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                      "~/Scripts/knockout-{version}.js",
+                      "~/Scripts/knockout.viewmodel.{version}.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
